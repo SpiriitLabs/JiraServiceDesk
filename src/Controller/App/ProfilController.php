e8b81b0dd9155378b5b7b14417b5d74a099a07e1
Romain Millan <romainmillan34560@gmail.com>
2025-04-07 19:35:37 +0200

    feat: User profile

---
+++ src/Controller/App/ProfilController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\App;
+
+use App\Controller\Common\EditControllerTrait;
+use App\Entity\User;
+use App\Form\Admin\User\UserProfileFormType;
+use App\Message\Command\User\EditUser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+#[Route(
+    path: '/profile',
+    name: RouteCollection::PROFIL->value,
+    methods: [Request::METHOD_GET, Request::METHOD_POST],
+)]
+class ProfilController extends AbstractController
+{
+    use EditControllerTrait;
+
+    public function __invoke(
+        Request $request,
+        #[CurrentUser]
+        User $user,
+    ): Response {
+        $form = $this->createForm(UserProfileFormType::class, new EditUser($user));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
+
+        return $this->render(
+            view: 'app/user/profil.html.twig',
+            parameters: [
+                'form' => $form->createView(),
+            ],
+        );
+    }
+}
