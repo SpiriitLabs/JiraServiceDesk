054a53e0c4694e196e80a60386e80d0b7b95feed
Romain Millan <romainmillan34560@gmail.com>
2025-05-11 16:24:45 +0200

    feat(admin): Dashboard update

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\ProjectRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +16,20 @@ use Symfony\Component\Routing\Attribute\Route;
 )]
 class DashboardController extends AbstractController
 {
+    public function __construct(
+        private readonly ProjectRepository $projectRepository,
+        private readonly UserRepository $userRepository,
+    ){}
+
     public function __invoke(): Response
     {
         return $this->render(
             view: 'admin/dashboard.html.twig',
+            parameters: [
+                'projects_count' => $this->projectRepository->count(),
+                'users_count' => $this->userRepository->count(),
+                'user_preference_notification' => $this->userRepository->createQueryBuilder('u')->select('COUNT(u.id)')->where('u.preferenceNotification = :active')->setParameter('active', true)->getQuery()->getSingleScalarResult(),
+            ],
         );
     }
 }
