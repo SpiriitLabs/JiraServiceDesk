ab20d2c6c3d3dbd38b99fce2afb08578ebe9fea7
Romain Millan <romainmillan34560@gmail.com>
2025-05-18 07:46:16 +0200

    turbo for board view

--- src/Controller/App/Project/BoardViewController.php
+++ src/Controller/App/Project/BoardViewController.php
@@ -10,29 +10,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Turbo\TurboBundle;
 
 #[Route(
     path: '/project/{idProject}/board/{idBoard}/view',
-    name: RouteCollection::PROJECT_BOARD_VIEW->value,
-    methods: [Request::METHOD_GET],
 )]
 class BoardViewController extends AbstractController
 {
     use GetControllerTrait;
 
-    public function __invoke(
+    #[Route(
+        path: '/',
+        name: RouteCollection::PROJECT_BOARD_VIEW->value,
+        methods: [Request::METHOD_GET],
+    )]
+    public function view(
         #[MapEntity(mapping: [
             'idProject' => 'id',
         ])]
         Project $project,
         string $idBoard,
     ): Response {
+        return $this->render(
+            view: 'app/project/board_view.html.twig',
+            parameters: [
+                'entity' => $project,
+                'boardId' => $idBoard,
+            ],
+        );
+    }
+
+    #[Route(
+        path: '/stream',
+        name: RouteCollection::PROJECT_BOARD_VIEW_STREAM->value,
+        methods: [Request::METHOD_GET],
+    )]
+    public function viewStream(
+        #[MapEntity(mapping: [
+            'idProject' => 'id',
+        ])]
+        Project $project,
+        string $idBoard,
+        Request $request,
+    ): Response {
+        $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
         $kanbanIssuesFormatted = $this->handle(
             new GetKanbanIssueByBoardId($project, $idBoard),
         );
 
         return $this->render(
-            view: 'app/project/board_view.html.twig',
+            view: 'app/project/board_view.stream.html.twig',
             parameters: [
                 'entity' => $project,
                 'boardId' => $idBoard,
