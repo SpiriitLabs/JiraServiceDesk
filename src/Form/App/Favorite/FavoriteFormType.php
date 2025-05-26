69a28cc750ef5636bd6c481e0cb94fd170a6f43e
Romain MILLAN <rmillan@spiriit.com>
2025-05-26 16:14:20 +0200

    feat(favorite): Use Turbo for favorites

---
+++ src/Form/App/Favorite/FavoriteFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\App\Favorite;
+
+use App\Controller\App\Favorite\RouteCollection;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class FavoriteFormType extends AbstractType
+{
+    public function __construct(
+        private readonly RouterInterface $router,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('code', HiddenType::class)
+            ->add('name', HiddenType::class)
+            ->add('link', HiddenType::class)
+            ->setAction(
+                $this->router->generate(
+                    RouteCollection::FAVORITE_STREAM->prefixed(),
+                    [
+                        'code' => $builder->getData()['code'],
+                    ]
+                )
+            )
+        ;
+    }
+}
