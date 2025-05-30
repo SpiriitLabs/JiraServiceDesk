5cb4aa99dfa9162ed336530d506dc791e7354fde
Romain MILLAN <rmillan@spiriit.com>
2025-05-30 12:44:07 +0200

    feat(issue): Comment author parse last text name

--- src/Twig/Extensions/JiraIssueExtension.php
+++ src/Twig/Extensions/JiraIssueExtension.php
@@ -17,11 +17,25 @@ class JiraIssueExtension extends AbstractExtension
     {
         return [
             new TwigFilter('preview_attachment', $this->previewAttachmentFormat(...)),
+            new TwigFilter('parse_comment_author', $this->parseCommentAuthor(...)),
         ];
     }
 
     public function previewAttachmentFormat($renderedDescription): string
     {
         return $this->htmlProcessor->updateImageSources($renderedDescription);
+    }
+
+    public function parseCommentAuthor($comment): ?string
+    {
+        $parts = explode('—', html_entity_decode($comment->renderedBody));
+        if (count($parts) > 1) {
+            $author = trim(array_pop($parts));
+            $author = strip_tags(str_ireplace(['<br>', '<br/>', '<br />'], '', $author));
+
+            return $author;
+        }
+
+        return null;
     }
 }
