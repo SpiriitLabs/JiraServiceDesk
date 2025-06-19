1e1d1f156c4893ef6f1518a6f41cd7fd2c869d0a
Romain Millan <romainmillan34560@gmail.com>
2025-06-19 16:59:47 +0200

    fix(user): Login check project user number allow to connect but not working for admin

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -4,6 +4,8 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Enum\User\Locale;
+use App\Enum\User\Role;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -13,6 +15,7 @@ class UserChecker implements UserCheckerInterface
 {
     public function __construct(
         private readonly TranslatorInterface $translator,
+        private readonly Security $security,
     ) {
     }
 
@@ -34,7 +37,10 @@ class UserChecker implements UserCheckerInterface
             ), );
         }
 
-        if ($user->getProjects()->count() == 0) {
+        if (($this->security->isGrantedForUser(
+            $user,
+            Role::ROLE_ADMIN
+        ) == false) && $user->getProjects()->count() == 0) {
             throw new CustomUserMessageAccountStatusException(message: $this->translator->trans(
                 id: 'security.login.user_account_no_projects',
                 domain: 'app',
