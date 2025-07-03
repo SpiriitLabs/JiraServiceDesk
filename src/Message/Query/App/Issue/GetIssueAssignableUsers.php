32508b5eb542979c5f82b42d4c40adef4eb8ecd9
Romain MILLAN <romainmillan34560@gmail.com>
2025-07-03 14:47:52 +0200

    feat(assignable): Custom current user in assignable list (#54)
    Co-authored-by: Anthony Delgado <adelgado@spiriit.com>

--- src/Message/Query/App/Issue/GetIssueAssignableUsers.php
+++ src/Message/Query/App/Issue/GetIssueAssignableUsers.php
@@ -3,10 +3,12 @@
 namespace App\Message\Query\App\Issue;
 
 use App\Entity\Project;
+use App\Entity\User;
 
 class GetIssueAssignableUsers
 {
     public function __construct(
+        public User $user,
         public ?Project $project = null,
     ) {
     }
