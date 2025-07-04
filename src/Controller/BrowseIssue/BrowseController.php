6c6fb495c0009ef422cbd14b43057f72aea49a4b
Romain MILLAN <rmillan@spiriit.com>
2025-07-04 07:53:51 +0200

    feat: Add focusCommentId param add to redirect route

--- src/Controller/BrowseIssue/BrowseController.php
+++ src/Controller/BrowseIssue/BrowseController.php
@@ -6,12 +6,15 @@ use App\Controller\App\Project\AbstractController;
 use App\Controller\App\Project\Issue\RouteCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 
 class BrowseController extends AbstractController
 {
     public function __invoke(
         Request $request,
         string $keyIssue,
+        #[MapQueryParameter]
+        ?string $focusedCommentId = null,
     ): Response {
         if (! preg_match('/^[A-Za-z0-9]+-\d+$/', $keyIssue)) {
             throw new \InvalidArgumentException(message: 'Invalid issue key format');
@@ -23,6 +26,7 @@ class BrowseController extends AbstractController
             parameters: [
                 'key' => $projectKey,
                 'keyIssue' => $keyIssue,
+                'focusedCommentId' => $focusedCommentId,
             ]
         );
     }
