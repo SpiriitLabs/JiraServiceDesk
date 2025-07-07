0b0ba31ae06eafe9a48b7b7ae12df35112547b3f
Romain MILLAN <romainmillan34560@gmail.com>
2025-07-07 10:49:01 +0200

    fix(comment): Fix name when is an jira account comment (#59)

---
+++ src/Twig/Extensions/IsJiraAccountIdExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Twig\Extensions;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Twig\Attribute\AsTwigFunction;
+
+class IsJiraAccountIdExtension
+{
+    public function __construct(
+        #[Autowire(env: 'JIRA_ACCOUNT_ID')]
+        private string $jiraAPIAccountId,
+    ) {
+    }
+
+    #[AsTwigFunction(
+        name: 'is_jira_account_id'
+    )]
+    public function isJiraAccountId(string|int $currentAccountId): bool
+    {
+        return $currentAccountId == $this->jiraAPIAccountId;
+    }
+}
