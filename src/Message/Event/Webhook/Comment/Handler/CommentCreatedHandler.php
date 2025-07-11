bea05c0211b33dd8affbd1d7ca2e66913c02f1d2
delny <delgado.anthony@gmail.com>
2025-07-11 08:38:48 +0200

    feat(notification) - replace account id by display name (#82)
    Co-authored-by: Anthony Delgado <adelgado@spiriit.com>

--- src/Message/Event/Webhook/Comment/Handler/CommentCreatedHandler.php
+++ src/Message/Event/Webhook/Comment/Handler/CommentCreatedHandler.php
@@ -6,6 +6,7 @@ use App\Message\Command\Common\EmailNotification;
 use App\Message\Event\Webhook\Comment\CommentCreated;
 use App\Repository\Jira\IssueRepository;
 use App\Repository\ProjectRepository;
+use App\Service\ReplaceAccountIdByDisplayName;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -25,6 +26,7 @@ class CommentCreatedHandler implements LoggerAwareInterface
         private readonly ProjectRepository $projectRepository,
         private readonly TranslatorInterface $translator,
         private readonly IssueRepository $issueRepository,
+        private readonly ReplaceAccountIdByDisplayName $replaceAccountIdByDisplayName,
         #[Autowire(env: 'JIRA_ACCOUNT_ID')]
         private string $jiraAPIAccountId,
     ) {
@@ -55,6 +57,7 @@ class CommentCreatedHandler implements LoggerAwareInterface
         ]);
 
         $commentBody = $event->getPayload()['comment']['body'];
+        $commentBody = $this->replaceAccountIdByDisplayName->replaceInCommentBody($commentBody);
         $templatedEmail = (new TemplatedEmail())
             ->htmlTemplate('email/comment/created.html.twig')
             ->context([
