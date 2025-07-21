b50473b6c3631b1ea1d817ae93d4f1bb1765afa4
Anthony Delgado <adelgado@spiriit.com>
2025-07-21 17:26:04 +0200

    fix(notif): Fix missing some replacements

--- src/Service/ReplaceAccountIdByDisplayName.php
+++ src/Service/ReplaceAccountIdByDisplayName.php
@@ -13,7 +13,7 @@ class ReplaceAccountIdByDisplayName
 
     public function replaceInCommentBody(string $comment): string
     {
-        if (preg_match_all('#\[~accountid:([a-zA-Z0-9\-]+)\]#', $comment, $matches)) {
+        if (preg_match_all('#\[~accountid:([a-zA-Z0-9\-:]+)\]#', $comment, $matches)) {
             $test = array_combine($matches[0], $matches[1]);
             foreach ($test as $match => $id) {
                 $account = $this->userRepository->getUserById($id);
