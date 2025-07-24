c588bf658ac2353296911293c41970d454c2a7df
Anthony Delgado <adelgado@spiriit.com>
2025-07-24 12:54:30 +0200

    move images and use static only to pwa

--- assets/app.ts
+++ assets/app.ts
@@ -1,5 +1,10 @@
 import './stimulus/bootstrap.ts';
 
+const images = import.meta.glob('./images/**/*', {
+    eager: true,
+    import: 'default',
+});
+
 import './styles/app.scss';
 import './theme';
 
