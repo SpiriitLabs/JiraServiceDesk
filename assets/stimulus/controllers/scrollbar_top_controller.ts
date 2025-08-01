f66a48829cb1a49f34cfd57ac61d47992ff1b76a
Anthony Delgado <adelgado@spiriit.com>
2025-08-01 15:10:23 +0200

    feat(kanban): Add scrollbar top

---
+++ assets/stimulus/controllers/scrollbar_top_controller.ts
@@ -0,0 +1,40 @@
+import { Controller } from '@hotwired/stimulus';
+
+
+export default class extends Controller {
+
+  static targets = ["select"];
+
+  declare readonly selectTarget: HTMLSelectElement;
+
+  connect(): void {
+    console.log("📋 Scrollbar Top controller connected.");
+
+    const scrollbarTop = document.getElementById('scrollbar-top');
+    if (scrollbarTop) {
+      scrollbarTop.addEventListener('scroll', () => {
+        const target = document.getElementById('scrollbar-bottom');
+        if (target) {
+          target.scrollLeft = scrollbarTop.scrollLeft;
+        }
+      });
+    }
+
+    const scrollbarBottom = document.getElementById('scrollbar-bottom');
+    if (scrollbarBottom) {
+      scrollbarBottom.addEventListener('scroll', () => {
+        const target = document.getElementById('scrollbar-top');
+        if (target) {
+          target.scrollLeft = scrollbarBottom.scrollLeft;
+        }
+      });
+      // init top width.
+      const topInner = document.getElementById('scrollbar-top-inner');
+      if (topInner && '' == topInner.style.width) {
+        topInner.style.width = scrollbarBottom.scrollWidth + 'px';
+      }
+    }
+  }
+
+}
+
