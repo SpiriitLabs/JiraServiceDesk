42dc7f35eefc3341d33de9d292339dbb667a0b92
Anthony Delgado <adelgado@spiriit.com>
2025-08-15 11:25:58 +0200

    feat(tests): User profile form type unit test

---
+++ tests/Unit/Form/Admin/User/UserProfileFormTypeTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Tests\Unit\Form\Admin\User;
+
+use App\Entity\Project;
+use App\Enum\User\Locale;
+use App\Enum\User\Theme;
+use App\Factory\UserFactory;
+use App\Form\Admin\User\UserProfileFormType;
+use App\Message\Command\User\EditUser;
+use App\Repository\ProjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Validator\Validation;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\UX\TogglePassword\Form\TogglePasswordTypeExtension;
+use Zenstruck\Foundry\Test\Factories;
+
+class UserProfileFormTypeTest extends TypeTestCase
+{
+    use Factories;
+
+    protected function getExtensions(): array
+    {
+        $validator = Validation::createValidator();
+        $translator = $this->createMock(TranslatorInterface::class);
+
+        // Mock ProjectRepository
+        $queryBuilder = $this->createMock(QueryBuilder::class);
+        $queryBuilder
+            ->method('getParameters')
+            ->willReturn(new ArrayCollection([]))
+        ;
+        $query = $this->createMock(Query::class);
+        $query
+            ->method('execute')
+            ->willReturn([])
+        ;
+        $queryBuilder
+            ->method('getQuery')
+            ->willReturn($query)
+        ;
+
+        $projectRepository = $this->createMock(ProjectRepository::class);
+        $projectRepository->method('getByUser')
+            ->willReturn($queryBuilder)
+        ;
+
+        // Mock EntityManager
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->method('getRepository')
+            ->willReturn($projectRepository)
+        ;
+        $em->method('getClassMetadata')
+            ->willReturn(new ClassMetadata(Project::class))
+        ;
+
+        // Mock ManagerRegistry
+        $managerRegistry = $this->createMock(ManagerRegistry::class);
+        $managerRegistry->method('getManagerForClass')
+            ->willReturn($em)
+        ;
+
+        return [
+            new PreloadedExtension(
+                [
+                    new UserProfileFormType(),
+                    new PasswordType(),
+                ],
+                [
+                    PasswordType::class => [
+                        new TogglePasswordTypeExtension($translator),
+                    ],
+                ]
+            ),
+            new ValidatorExtension($validator),
+            new DoctrineOrmExtension($managerRegistry),
+        ];
+    }
+
+    #[Test]
+    public function testSubmitValidData(): void
+    {
+        $formData = [
+            'preferedLocale' => Locale::FR->value,
+            'preferedTheme' => Theme::AUTO->value,
+            'firstName' => 'Pierre',
+            'lastName' => 'DUPONT',
+            'preferenceNotification' => true,
+            'preferenceNotificationIssueCreated' => true,
+            'preferenceNotificationIssueUpdated' => true,
+            'preferenceNotificationCommentUpdated' => true,
+            'preferenceNotificationCommentCreated' => true,
+        ];
+
+        $user = UserFactory::createOne([
+            'email' => 'test@local.lan',
+            'firstName' => 'Alain',
+            'lastName' => 'DUPONT',
+            'preferredLocale' => Locale::FR,
+            'preferredTheme' => Theme::AUTO,
+            'preferenceNotification' => true,
+            'preferenceNotificationIssueCreated' => true,
+            'preferenceNotificationIssueUpdated' => true,
+            'preferenceNotificationCommentUpdated' => true,
+            'preferenceNotificationCommentCreated' => true,
+        ]);
+        $updateUser = (clone $user);
+        $updateUser->setLastName('DUPONT');
+        $updateUser->setFirstName('Pierre');
+
+        $model = new EditUser(
+            user: $user,
+        );
+
+        $form = $this->factory->create(
+            type: UserProfileFormType::class,
+            data: $model,
+        );
+
+        $expected = new EditUser(
+            user: $updateUser,
+        );
+
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($expected, $model);
+    }
+}
