a99e3e3f6f5293cf9e5520d1210287373ef4a375
Romain MILLAN <romainmillan34560@gmail.com>
2025-10-03 17:20:33 +0200

    refacto(logs): Add somes properties + Subscriber update for mails (#52)

---
+++ src/Factory/LogEntryFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\LogEntry;
+use App\Enum\LogEntry\Level;
+use App\Enum\LogEntry\Type;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<LogEntry>
+ */
+final class LogEntryFactory extends PersistentProxyObjectFactory
+{
+    public static function class(): string
+    {
+        return LogEntry::class;
+    }
+
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'type' => self::faker()->randomElement(Type::cases()),
+            'level' => self::faker()->randomElement(Level::cases()),
+            'subject' => self::faker()->text(),
+            'datas' => [
+                'key' => self::faker()->text(20),
+                'another_key' => self::faker()->text(20),
+                'integer' => self::faker()->randomNumber(),
+                'float' => self::faker()->randomFloat(2, 0, 1000),
+                'boolean' => self::faker()->boolean(),
+            ],
+        ];
+    }
+
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function (LogEntry $logEntry): void {
+                $reflection = new \ReflectionClass($logEntry);
+                $property = $reflection->getProperty('logAt');
+                $property->setAccessible(true);
+                $property->setValue($logEntry, \DateTimeImmutable::createFromMutable(self::faker()->dateTime()));
+            })
+        ;
+    }
+}
--- src/Message/Command/App/Notification/Handler/CreateNotificationHandler.php
+++ src/Message/Command/App/Notification/Handler/CreateNotificationHandler.php
@@ -4,14 +4,18 @@ namespace App\Message\Command\App\Notification\Handler;
 
 use App\Entity\Notification;
 use App\Message\Command\App\Notification\CreateNotification;
+use App\Subscriber\Event\NotificationEvent;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[AsMessageHandler]
 readonly class CreateNotificationHandler
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private MailerInterface $mailer,
+        private EventDispatcherInterface $dispatcher,
     ) {
     }
 
@@ -20,7 +24,27 @@ readonly class CreateNotificationHandler
         if ($command->user->enabled === false) {
             return null;
         }
-        $notification = new Notification(
+
+        if ($command->email) {
+            $this->mailer->send(
+                $command->email,
+            );
+
+            $this->dispatcher->dispatch(
+                new NotificationEvent(
+                    user: $command->user,
+                    message: sprintf('Notification email sent to "%s"', $command->user->email),
+                    extraData: [
+                        'subject' => $command->subject,
+                        'body' => $command->body,
+                        'link' => $command->link,
+                    ],
+                ),
+                NotificationEvent::EVENT_NAME,
+            );
+        }
+
+        $notification = new NotificationEntity(
             notificationType: $command->notificationType,
             subject: $command->subject,
             body: $command->body,
