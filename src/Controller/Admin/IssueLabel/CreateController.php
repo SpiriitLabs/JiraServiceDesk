84e729f4826b28b89004eb762d3ca21adf979c07
Anthony Delgado <adelgado@spiriit.com>
2025-11-06 06:35:56 +0100

    Check label already exist

--- src/Controller/Admin/IssueLabel/CreateController.php
+++ src/Controller/Admin/IssueLabel/CreateController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Admin\IssueLabel;
 
 use App\Controller\Common\CreateControllerTrait;
 use App\Entity\IssueLabel;
+use App\Exception\Project\IssueLabelAlreadyExistException;
 use App\Form\Admin\IssueLabel\IssueLabelFormType;
 use App\Message\Command\Admin\IssueLabel\CreateIssueLabel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,6 +35,15 @@ class CreateController extends AbstractController
                 /** @var ?IssueLabel $issueLabelCreated */
                 $issueLabelCreated = $this->handle($form->getData());
             } catch (HandlerFailedException $exception) {
+                if ($exception->getPrevious() instanceof IssueLabelAlreadyExistException) {
+                    $this->addFlash(
+                        type: 'danger',
+                        message: $exception->getPrevious()
+                            ->getMessage(),
+                    );
+
+                    return $this->redirectToRoute(RouteCollection::CREATE->prefixed());
+                }
                 throw $exception;
             }
 
--- src/Message/Command/Admin/IssueLabel/Handler/CreateIssueLabelHandler.php
+++ src/Message/Command/Admin/IssueLabel/Handler/CreateIssueLabelHandler.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Message\Command\Admin\IssueLabel\Handler;
 
 use App\Entity\IssueLabel;
+use App\Exception\Project\IssueLabelAlreadyExistException;
 use App\Message\Command\Admin\IssueLabel\CreateIssueLabel;
+use App\Repository\IssueLabelRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
@@ -14,11 +16,18 @@ readonly class CreateIssueLabelHandler
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private IssueLabelRepository $issueLabelRepository,
     ) {
     }
 
     public function __invoke(CreateIssueLabel $command): ?IssueLabel
     {
+        if ($this->issueLabelRepository->findOneBy([
+            'jiraLabel' => $command->jiraLabel,
+        ]) !== null) {
+            throw new IssueLabelAlreadyExistException();
+        }
+
         $issueLabel = new IssueLabel(
             jiraLabel: $command->jiraLabel,
             name: $command->name,
