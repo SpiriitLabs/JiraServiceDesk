964b1cf64ee0ff9a6f42d06ba5e8d51cb2d5ff49
Anthony Delgado <adelgado@spiriit.com>
2025-11-19 11:40:50 +0100

    feat(cache): Add cache on project api endpoint

--- src/Message/Query/App/Project/Handler/GetProjectRolesByJiraKeyHandler.php
+++ src/Message/Query/App/Project/Handler/GetProjectRolesByJiraKeyHandler.php
@@ -6,11 +6,14 @@ namespace App\Message\Query\App\Project\Handler;
 
 use App\Message\Query\App\Project\GetProjectRolesByJiraKey;
 use App\Repository\Jira\ProjectRepository;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-class GetProjectRolesByJiraKeyHandler
+readonly class GetProjectRolesByJiraKeyHandler
 {
+    protected const int CACHE_DURATION = 7200;
+
     public function __construct(
         private ProjectRepository $projectRepository,
     ) {
@@ -18,12 +21,23 @@ class GetProjectRolesByJiraKeyHandler
 
     public function __invoke(GetProjectRolesByJiraKey $query): array
     {
+        $cache = new FilesystemAdapter();
+        $cacheJiraRoles = $cache->getItem(sprintf('jira.project_roles_%s', $query->jiraKey));
+
+        if ($cacheJiraRoles->isHit()) {
+            return $cacheJiraRoles->get();
+        }
+
         $jiraRoles = $this->projectRepository->getRoles($query->jiraKey);
 
         foreach ($jiraRoles as $jiraRoleName => $jiraRoleSelf) {
             $jiraRoleSelfArray = explode('/', rtrim($jiraRoleSelf, '/'));
             $jiraRoles[$jiraRoleName] = end($jiraRoleSelfArray);
         }
+
+        $cacheJiraRoles->set($jiraRoles);
+        $cacheJiraRoles->expiresAfter(self::CACHE_DURATION);
+        $cache->save($cacheJiraRoles);
 
         return $jiraRoles;
     }
